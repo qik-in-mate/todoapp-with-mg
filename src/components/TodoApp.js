f98295776961099c6dd346dd76914c97ac7277de
Ievgenii.Nikitchenko <nakyola@gmail.com>
2019-06-18 13:56:16 +0300

    Summary

---
+++ src/components/TodoApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TodoApp extends React.Component {
+
+  render() {
+    return (
+      <div>
+        TODOApp will be here
+        <h2>Hello</h2>
+      </div>
+    );
+  };
+}
+
+export default TodoApp;
