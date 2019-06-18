584e2304b9294ae42cd084b37fb0db27180a9b8d
Ievgenii.Nikitchenko <nakyola@gmail.com>
2019-06-18 14:09:49 +0300

    Updates

---
+++ precache-manifest.3466ff0ff843232bcbc755763b648257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c01de283e7ef7b8a393d8bc745256c65",
+    "url": "/todoapp-with-mg/index.html"
+  },
+  {
+    "revision": "08f5309f4bd730bd8ac5",
+    "url": "/todoapp-with-mg/static/css/main.97ff4449.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/todoapp-with-mg/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "08f5309f4bd730bd8ac5",
+    "url": "/todoapp-with-mg/static/js/main.b86a8246.chunk.js"
+  },
+  {
+    "revision": "61e807ca4db9a7cc959a",
+    "url": "/todoapp-with-mg/static/js/runtime~main.230e8973.js"
+  }
+]);
